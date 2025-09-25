107789028869581a02e1ac5cc34809ab9670becb
joongwon <anjoongwon517@gmail.com>
2025-09-25 11:27:39 +0900

    컨트롤러 & 테스트 API - 공개 엔드포인트

--- src/main/java/com/sprint/team2/customfilterlab/web/PublicController.java
+++ src/main/java/com/sprint/team2/customfilterlab/web/PublicController.java
@@ -2,7 +2,9 @@ package com.sprint.team2.customfilterlab.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PublicController {
     @GetMapping("/public/hello")
     public String hello(@RequestParam(defaultValue = "world") String name,
