5af9e29077123435f5490cd6ee54f45f813680aa
joongwon <anjoongwon517@gmail.com>
2025-09-25 10:56:23 +0900

    Initial commit

---
+++ src/test/java/com/sprint/team2/customfilterlab/CustomFilterLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.team2.customfilterlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomFilterLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
